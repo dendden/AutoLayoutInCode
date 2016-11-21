9e83511e8720ed06d5ff26216913680fd39f63b0
Denys Triasunov <dendden@users.noreply.github.com>
2016-11-21 18:52:03 +0200

    5 labels created in code with constraints set with either VFL and Anchors

--- AutoLayoutCode/ViewController.swift
+++ AutoLayoutCode/ViewController.swift
@@ -12,7 +12,64 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        // Auto Layout Visual Format Language (VFL):
+        let viewsDict = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        let metricsDict = ["labelHeight": 88]
+        
+        for label in viewsDict.keys {
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDict))
+        }
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|->=10-[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metricsDict, views: viewsDict))
+        
+        //Auto Layout Anchors:
+//        var previousLabel: UILabel!
+//        
+//        for label in [label1, label2, label3, label4, label5] {
+//            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+//            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+//            
+//            if previousLabel != nil {
+//                label.topAnchor.constraint(equalTo: previousLabel.bottomAnchor).isActive = true
+//            }
+//            
+//            previousLabel = label
+//        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
